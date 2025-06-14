6897231894720e5683319cafb21d437bdae366c6
Olivier Boeren <olivierboeren@live.nl>
2025-06-14 22:32:31 +0200

    fix: tree-sitter not loading

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,26 @@
 return {
-	'nvim-treesitter/nvim-treesitter', 
-	opts={run = ':TSUpdate'}
+	'nvim-treesitter/nvim-treesitter',
+	opt={
+		branch='master'
+	},
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+		  ensure_installed = {
+			  "c",
+			  "lua",
+			  "vim",
+			  "vimdoc",
+			  "query",
+			  "elixir",
+			  "heex",
+			  "javascript",
+			  "html"},
+
+		  sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true }
+        })
+	end
 }
